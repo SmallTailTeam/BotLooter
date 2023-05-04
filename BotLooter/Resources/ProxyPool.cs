97f8683bc56e1b7497c3f6a649eb1346c30a1686
小鳥 <44732554+Lyrapuff@users.noreply.github.com>
2023-05-04 09:30:38 +0300

    Improve proxy parsing

--- BotLooter/Resources/ProxyPool.cs
+++ BotLooter/Resources/ProxyPool.cs
@@ -38,22 +38,39 @@ public class ProxyPool
         var lines = await File.ReadAllLinesAsync(filePath);
 
         var proxiedClients = new List<RestClient>();
+
+        var lineNumber = 0;
         
         foreach (var line in lines)
         {
-            var uri = new Uri(line);
-
-            var proxy = new WebProxy(uri);
-            var credentials = uri.UserInfo.Split(new[] { ':' }, 2);
-            proxy.Credentials = new NetworkCredential(credentials[0], credentials[1]);
-
-            var restClient = new RestClient(new RestClientOptions
-            {
-                UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
-                Proxy = proxy
-            }, configureSerialization: b => b.UseNewtonsoftJson());
+            lineNumber++;
             
-            proxiedClients.Add(restClient);
+            try
+            {
+                var uri = new Uri(line);
+
+                var proxy = new WebProxy(uri);
+
+                if (!string.IsNullOrEmpty(uri.UserInfo))
+                {
+                    var credentials = uri.UserInfo.Split(':', 2);
+                    
+                    proxy.Credentials = new NetworkCredential(credentials[0], credentials[1]);
+                }
+
+                var restClient = new RestClient(new RestClientOptions
+                {
+                    UserAgent =
+                        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
+                    Proxy = proxy
+                }, configureSerialization: b => b.UseNewtonsoftJson());
+
+                proxiedClients.Add(restClient);
+            }
+            catch
+            {
+                Console.WriteLine($"Неверный формат прокси на строке {lineNumber}: {line}");
+            }
         }
 
         return (new ProxyPool(proxiedClients), "");
