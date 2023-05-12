d96ff11f9ece0b647b64f97b0f5b37ba7f5e6cff
小鳥 <44732554+Lyrapuff@users.noreply.github.com>
2023-05-13 00:16:58 +0300

    Version bump to 0.1.2

--- BotLooter/Program.cs
+++ BotLooter/Program.cs
@@ -17,7 +17,7 @@ AppDomain.CurrentDomain.UnhandledException += (_, eventArgs) =>
     Console.ReadKey();
 };
 
-var version = new Version(0, 1, 1);
+var version = new Version(0, 1, 2);
 
 var versionChecker = new VersionChecker(Log.Logger);
 await versionChecker.Check(version);
