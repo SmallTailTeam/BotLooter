f545a63f04a3736824ceff1d255f9b6bc68ed9b7
Sadzurami <sadzurami2036@gmail.com>
2024-07-13 13:04:09 +0300

    fix: update user-agent

--- BotLooter/Resources/LocalRestClientProvider.cs
+++ BotLooter/Resources/LocalRestClientProvider.cs
@@ -14,7 +14,7 @@ public class LocalRestClientProvider : IRestClientProvider
         _restClient = new RestClient(new RestClientOptions
         {
             UserAgent =
-                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
             FollowRedirects = false,
             MaxTimeout = 60000
         }, 
